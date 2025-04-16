42bee4a78624f3cd07fd2449f99bcc2787a3433b
ZyphiaHub <149183538+ZyphiaHub@users.noreply.github.com>
2025-04-16 08:07:41 +0200

    kód tisztítás

---
+++ Gameapp/AstroWheel Játék Unity csak Script/HOG/RandomObjectSelector.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomObjectSelector : MonoBehaviour {
+    public int clickableObjectCount = 10;
+    [SerializeField] private HOGManager manager;
+
+    public void Initialize(List<GameObject> allObjects)
+    {
+        
+        List<HiddenObject> targets = new List<HiddenObject>();
+        List<HiddenObject> availableObjects = new List<HiddenObject>();
+        manager.SetTargetObjects(targets);
+
+        // Összes elérhetõ objektum gyûjtése
+        foreach (GameObject obj in allObjects)
+        {
+            HiddenObject hiddenObj = obj.GetComponent<HiddenObject>();
+            if (hiddenObj != null)
+            {
+                availableObjects.Add(hiddenObj);
+                hiddenObj.SetClickable(false); 
+            }
+        }
+
+        // Véletlenszerû kiválasztás
+        while (targets.Count < clickableObjectCount && availableObjects.Count > 0)
+        {
+            int randomIndex = Random.Range(0, availableObjects.Count);
+            HiddenObject selected = availableObjects[randomIndex];
+            targets.Add(selected);
+            availableObjects.RemoveAt(randomIndex);
+        }
+
+        foreach (HiddenObject obj in targets)
+        {
+            obj.SetClickable(true);
+        }
+        //ami nincs a listán ne legyen kattintható!
+        foreach (HiddenObject obj in availableObjects)
+        {
+            obj.SetClickable(false);
+        }
+
+        manager.SetTargetObjects(targets);
+    }
+}
