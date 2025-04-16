563f3ef98fbc1bfaeddafff90acd17f7a048035e
ZyphiaHub <149183538+ZyphiaHub@users.noreply.github.com>
2025-04-16 08:09:17 +0200

    kód tisztítás

---
+++ Gameapp/AstroWheel Játék Unity csak Script/Match3/DestroyParticle.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class DestroyParticle : MonoBehaviour {
+
+    private float delayTimer = 1;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        delayTimer -= Time.deltaTime;
+        if(delayTimer<= 0){
+            Destroy(this.gameObject);
+        }
+	}
+}
